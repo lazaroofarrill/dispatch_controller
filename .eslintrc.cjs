8441505a0a96373901ab397f70c8f6d78db8375c
lazaroofarrill <lazaroofarrill@gmail.com>
2023-06-02 03:55:51 -0400

    changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
   'rules': {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-unexpected-multiline': 'off',
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
